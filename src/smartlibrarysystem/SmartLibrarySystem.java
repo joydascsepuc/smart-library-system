9e2dfbd13d2ce2ad2a0b35fe0b70766a3125e7a2
JoyDas <joydascsepuc@gmail.com>
2020-12-10 01:21:25 +0600

    base project

---
+++ src/smartlibrarysystem/SmartLibrarySystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartlibrarysystem;
+
+/**
+ *
+ * @author hp
+ */
+public class SmartLibrarySystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Home mf = new Home();
+        mf.setVisible(true);
+        mf.setResizable(false);
+    }
+    
+}
